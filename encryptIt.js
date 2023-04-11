fc1402ed5aa0219eafaa0864516966930b329276
Serhat Kemal <97290537+srhtkemal@users.noreply.github.com>
2023-04-11 17:13:00 +0300

    Update encryptIt.js

--- encryptIt.js
+++ encryptIt.js
@@ -1,55 +1,66 @@
 
-  const encryptButton = document.getElementById('encrypt-it');
-  const resetButton = document.getElementById('reset');
-  const inputText = document.getElementById('input-text');
-  const resultArea = document.getElementById('resultArea');
-  const cipherType = document.getElementById('cipher-type');
-  const shiftAmount = document.getElementById('shift-amount');
-  const textSize12 = document.querySelector('input[value="12pt"]');
-  const textSize24 = document.querySelector('input[value="24pt"]');
-  const allCaps = document.getElementById('all-caps');
+const { 
+  value: inputTextValue 
+} = document.getElementById('input-text');
+const { 
+  value: cipherTypeValue 
+} = document.getElementById('cipher-type');
+const { 
+  value: shiftAmountValue 
+} = document.getElementById('shift-amount');
+const { 
+  checked: textSize12Checked 
+} = document.querySelector('input[value="12pt"]');
+const { 
+  checked: textSize24Checked 
+} = document.querySelector('input[value="24pt"]');
+const { 
+  checked: allCapsChecked 
+} = document.getElementById('all-caps');
 
-  function encryptText() {
-    const textToEncrypt = inputText.value;
-    const selectedCipherType = cipherType.value;
-    const selectedShiftAmount = parseInt(shiftAmount.value);
-    const selectedTextSize = textSize12.checked ? '12pt' : '24pt';
-    const shouldUppercase = allCaps.checked;
+const encryptButton = document.getElementById('encrypt-it');
+const resetButton = document.getElementById('reset');
+const resultArea = document.getElementById('result');
 
-    let encryptedText = '';
+const encryptText = () => {
+  const textToEncrypt = inputTextValue;
+  const selectedCipherType = cipherTypeValue;
+  const selectedShiftAmount = parseInt(shiftAmountValue);
+  const selectedTextSize = textSize12Checked ? '12pt' : '24pt';
+  const shouldUppercase = allCapsChecked;
+  let encryptedText = '';
 
-    if (selectedCipherType === 'shift') {
-      for (let i = 0; i < textToEncrypt.length; i++) {
-        let asciiCode = textToEncrypt.charCodeAt(i);
-        if (asciiCode >= 65 && asciiCode <= 90) {
-          asciiCode = ((asciiCode - 65 + selectedShiftAmount) % 26) + 65;
-        } else if (asciiCode >= 97 && asciiCode <= 122) {
-          asciiCode = ((asciiCode - 97 + selectedShiftAmount) % 26) + 97;
-        }
-        encryptedText += String.fromCharCode(asciiCode);
+  if (selectedCipherType === 'shift') {
+    for (let i = 0; i < textToEncrypt.length; i++) {
+      let asciiCode = textToEncrypt.charCodeAt(i);
+      if (asciiCode >= 65 && asciiCode <= 90) {
+        asciiCode = ((asciiCode - 65 + selectedShiftAmount) % 26) + 65;
+      } else if (asciiCode >= 97 && asciiCode <= 122) {
+        asciiCode = ((asciiCode - 97 + selectedShiftAmount) % 26) + 97;
       }
-    } else if (selectedCipherType === 'random') {
-      for (let i = 0; i < textToEncrypt.length; i++) {
-        let asciiCode = textToEncrypt.charCodeAt(i);
-        let randomShiftAmount = Math.floor(Math.random() * 26);
-        if (asciiCode >= 65 && asciiCode <= 90) {
-          asciiCode = ((asciiCode - 65 + randomShiftAmount) % 26) + 65;
-        } else if (asciiCode >= 97 && asciiCode <= 122) {
-          asciiCode = ((asciiCode - 97 + randomShiftAmount) % 26) + 97;
-        }
-        encryptedText += String.fromCharCode(asciiCode);
+      encryptedText += String.fromCharCode(asciiCode);
+    }
+  } else if (selectedCipherType === 'random') {
+    for (let i = 0; i < textToEncrypt.length; i++) {
+      let asciiCode = textToEncrypt.charCodeAt(i);
+      let randomShiftAmount = Math.floor(Math.random() * 26);
+      if (asciiCode >= 65 && asciiCode <= 90) {
+        asciiCode = ((asciiCode - 65 + randomShiftAmount) % 26) + 65;
+      } else if (asciiCode >= 97 && asciiCode <= 122) {
+        asciiCode = ((asciiCode - 97 + randomShiftAmount) % 26) + 97;
       }
+      encryptedText += String.fromCharCode(asciiCode);
     }
-
-    if (shouldUppercase) {
-      encryptedText = encryptedText.toUpperCase();
-    } else {
-      encryptedText = encryptedText.toLowerCase();
-    }
-
-    resultArea.style.fontSize = selectedTextSize;
-    resultArea.innerHTML = encryptedText;
   }
 
-  encryptButton.addEventListener('click', encryptText);
+  if (shouldUppercase) {
+    encryptedText = encryptedText.toUpperCase();
+  } else {
+    encryptedText = encryptedText.toLowerCase();
+  }
 
+  resultArea.style.fontSize = selectedTextSize;
+  resultArea.innerHTML = encryptedText;
+};
+
+encryptButton.addEventListener('click', encryptText);
